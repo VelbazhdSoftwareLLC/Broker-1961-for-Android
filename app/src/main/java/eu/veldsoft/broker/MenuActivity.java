64ce8719a7c40c73c95f03d2a2695839c6d935e5
Martinkata1 <marti.kostadinov2003@gmail.com>
2022-11-12 16:29:53 +0200

    Game windows have been created.

---
+++ app/src/main/java/eu/veldsoft/broker/MenuActivity.java
@@ -0,0 +1,14 @@
+package eu.veldsoft.broker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
