15cb0b2e260369f87db5e171a603ffb2e6138267
Martinkata1 <marti.kostadinov2003@gmail.com>
2022-11-15 19:49:30 +0200

    Game screens open.

--- app/src/main/java/eu/veldsoft/broker/GameActivity.java
+++ app/src/main/java/eu/veldsoft/broker/GameActivity.java
@@ -1,6 +1,10 @@
 package eu.veldsoft.broker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,33 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                break;
+            case R.id.help:
+                startActivity(new Intent(GameActivity.this, HelpActivity.class));
+                break;
+            case R.id.about:
+                startActivity(new Intent(GameActivity.this, AboutActivity.class));
+                break;
+        }
+        return true;
     }
 }
