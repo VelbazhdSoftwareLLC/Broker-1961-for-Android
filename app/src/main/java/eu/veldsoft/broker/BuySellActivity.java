655d881cbc86aebee1c8251826b3450e6af5baed
Martinkata1 <marti.kostadinov2003@gmail.com>
2023-01-19 20:36:53 +0200

    Some part of buy/sell has been done.

--- app/src/main/java/eu/veldsoft/broker/BuySellActivity.java
+++ app/src/main/java/eu/veldsoft/broker/BuySellActivity.java
@@ -1,8 +1,11 @@
 package eu.veldsoft.broker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +25,42 @@ public class BuySellActivity extends AppCompatActivity {
         ((Button) findViewById(R.id.tradeFinish)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int shares[] = {0, 0, 0, 0};
+
+                shares[0] = Integer.valueOf(((EditText) findViewById(R.id.aAmountNumber)).getText().toString());
+                shares[1] = Integer.valueOf(((EditText) findViewById(R.id.bAmountNumber)).getText().toString());
+                shares[2] = Integer.valueOf(((EditText) findViewById(R.id.cAmountNumber)).getText().toString());
+                shares[3] = Integer.valueOf(((EditText) findViewById(R.id.dAmountNumber)).getText().toString());
+
+                if (((RadioButton) findViewById(R.id.aBuyButton)).isChecked() == true) {
+                    shares[0] = Math.abs(shares[0]);
+                }
+                if (((RadioButton) findViewById(R.id.aSellButton)).isChecked() == true) {
+                    shares[0] = -Math.abs(shares[0]);
+                }
+                if (((RadioButton) findViewById(R.id.bBuyButton)).isChecked() == true) {
+                    shares[1] = Math.abs(shares[1]);
+                }
+                if (((RadioButton) findViewById(R.id.bSellButton)).isChecked() == true) {
+                    shares[1] = -Math.abs(shares[1]);
+                }
+                if (((RadioButton) findViewById(R.id.cBuyButton)).isChecked() == true) {
+                    shares[2] = Math.abs(shares[2]);
+                }
+                if (((RadioButton) findViewById(R.id.cSellButton)).isChecked() == true) {
+                    shares[2] = -Math.abs(shares[2]);
+                }
+                if (((RadioButton) findViewById(R.id.dBuyButton)).isChecked() == true) {
+                    shares[3] = Math.abs(shares[3]);
+                }
+                if (((RadioButton) findViewById(R.id.dSellButton)).isChecked() == true) {
+                    shares[3] = -Math.abs(shares[3]);
+                }
+
+                Intent intent = new Intent();
+                intent.putExtra("buySellShares", shares);
+                setResult(AppCompatActivity.RESULT_OK, intent);
+
                 BuySellActivity.this.finish();
             }
         });
